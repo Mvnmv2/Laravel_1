48c8b778665c2aa2e8eec649c0a21b30bcd662e2
Andrey <maven23@rambler.ru>
2023-02-08 17:56:47 +0300

    add bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,20 +5,29 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/bootstrap/bootstrap-5.3.0-alpha1-dist/css/bootstrap.css">
     <title>Document</title>
 </head>
 <body>
-<div>
-    <nav>
-        <ul>
-            <li><a href="{{route('main.index')}}">Main</a></li>
-            <li><a href="{{route('post.index')}}">Posts</a></li>
-            <li><a href="{{route('contact.index')}}">Contact</a></li>
-            <li><a href="{{route('about.index')}}">About</a></li>
-
-        </ul>
-    </nav>
+<div class="container mt-5">
+    <div class="row">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="{{route('main.index')}}">Main</a>
+                        <a class="nav-link" href="{{route('post.index')}}">Posts</a>
+                        <a class="nav-link" href="{{route('contact.index')}}">Contact</a>
+                        <a class="nav-link" href="{{route('about.index')}}">About</a>
+                        <a class="nav-link disabled">Disabled</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+    @yield('content')
 </div>
-@yield('content')
+
+<script src="/bootstrap/bootstrap-5.3.0-alpha1-dist/js/bootstrap.js"></script>
 </body>
 </html>
